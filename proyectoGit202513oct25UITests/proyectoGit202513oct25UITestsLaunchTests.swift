2fb09b678f30b68817983da06e7b733315ee712b
Miguel Angel Casillas Lopez <casillaslopez@gmail.com>
2025-10-13 07:56:31 -0600

    Initial Commit

---
+++ proyectoGit202513oct25UITests/proyectoGit202513oct25UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  proyectoGit202513oct25UITestsLaunchTests.swift
+//  proyectoGit202513oct25UITests
+//
+//  Created by Miguel on 13/10/25.
+//
+
+import XCTest
+
+final class proyectoGit202513oct25UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
